da71808cd34f723cc4333395bf8d2281fb2548fc
nimuC <nimuC@outlook.com>
2020-05-29 21:16:57 +0900

    add: vote command. Make player possible to vote on discord!

--- cogs/game.py
+++ cogs/game.py
@@ -1,6 +1,8 @@
+import discord
 from discord.ext import commands
 from lib.death_game import DeathGame
 from lib.player import Player
+from typing import Union
 
 
 class Game(commands.Cog):
@@ -18,6 +20,27 @@ class Game(commands.Cog):
     @commands.command()
     async def ranking(self, ctx):
         await ctx.channel.send('', embed=self.game.embed_votes_ranking())
+
+    @commands.command()
+    async def vote(
+        self,
+        ctx,
+        voted_discord_account: Union[discord.Member, discord.User]
+    ):
+        voter = self.game.player_by_discord(ctx.author)
+        voted_player = self.game.player_by_discord(voted_discord_account)
+
+        if not voter.has_voting_rights():
+            await ctx.channel.send('投票権がありません。')
+            return
+
+        if voted_player is None:
+            await ctx.channel.send('投票先が見つかりませんでした。')
+            return
+
+        self.game = self.game.vote(voter, voted_player)
+
+        await ctx.channel.send('投票しました。')
 
 
 def setup(bot):
